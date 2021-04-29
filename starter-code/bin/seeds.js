b918424a4ad7c79467031ec7d9bf33a90d2212ee
Gaston <gaston@github.com>
2021-04-29 16:28:38 +0200

    lab-mongoose-movies initial commit

---
+++ starter-code/bin/seeds.js
@@ -0,0 +1,23 @@
+
+const Celebrity = require('../models/celebrity.js');
+
+const celebrities = [
+    {name:'Adam Sandler', occupation:'Actor', catchPhrase: 'Oh really, fool?'},
+    {name:'Ben Stiller ', occupation:'Actor', catchPhrase: 'I got it Stuck!'},
+    {name: 'Jenifer Aniston', occupation:'Actrees', catchPhrase: 'I feel sexy'}
+
+];
+
+const mongoose = require('mongoose')
+mongoose.connect('mongodb://localhost/mongoose-movies'), {
+    useCreateIndex: true,
+    useNewUrlParser: true,
+    useUnifiedTopology: true
+}
+
+Celebrity.create(celebrities)
+.then((feedback) => {
+    console.log(feedback)
+    mongoose.connection.close()
+})
+.catch(error => console.log('An erro ocurred while creating celebrities', error ))
