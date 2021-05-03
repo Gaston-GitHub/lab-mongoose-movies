1a312831b302e440b8a96496220f1bf893048436
Gaston <gaston@github.com>
2021-05-03 18:12:52 +0200

    lab-mongoose-movies it 1 2 3

--- starter-code/routes/celebrities.js
+++ starter-code/routes/celebrities.js
@@ -1,14 +1,28 @@
 const express = require('express')
-const Celebrity = require('../models/Celebrity.model');
-
 const router = express.Router();
 
-router.get('/index', (req, res, next) => {
+const Celebrity = require('../models/Celebrity.model');
+
+
+
+router.get('/celebrities', (req, res, next) => {
     Celebrity.find({})
     .then((celebrities) => {
         res.render('celebrities/index', {celebrities})
     })
     .catch(error => { 
+        next(error)
+    })
+})
+
+router.get('/celebrities/:id', (req, res, next) => {
+    const id = req.params.id
+    Celebrity.findById(id)
+    .then((celebrity) => {
+        console.log('celebrity', celebrity)
+        res.render('celebrities/show', {celebrity})
+    })
+    .catch(error => {
         next(error)
     })
 })
